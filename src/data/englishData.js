6b2a356e28fd5ea153bbef53dd655f221c90393e
Islam <ikabden04@gmail.com>
2026-01-20 16:48:11 +0500

    add 5 words in english

--- src/data/englishData.js
+++ src/data/englishData.js
@@ -1,85 +1,30 @@
 export const questionsEnglish = [
     {
         id: 1,
-        questions: "Bind",
-        answer: "Связывать",
+        questions: "Bulk up",
+        answer: "Качать мыщцы",
         period: 1
     },{
         id: 2,
-        questions: "Approximate",
-        answer: "Приблизительный",
+        questions: "Praise",
+        answer: "Хвалить",
         period: 1
     },{
         id: 3,
-        questions: "Technique",
-        answer: "Метод",
+        questions: "Aware",
+        answer: "Осведомленный",
         period: 1
     },{
         id: 4,
-        questions: "Rearrangement",
-        answer: "Перестройка",
+        questions: "Irresstible",
+        answer: "Неотрозимый",
         period: 1
     },{
         id: 5,
-        questions: "Lead",
-        answer: "Приводить",
+        questions: "Seashare",
+        answer: "Берег моря",
         period: 1
-    },{
-        id: 5,
-        questions: "Various",
-        answer: "Различный",
-        period: 1
-    },{
-        id: 6,
-        questions: "Available",
-        answer: "Доступный",
-        period: 1
-    },{
-        id: 7,
-        questions: "Involve",
-        answer: "Вовлекать",
-        period: 1
-    },{
-        id: 8,
-        questions: "Simple",
-        answer: "Простой",
-        period: 1
-    },{
-        id: 9,
-        questions: "Appropriate",
-        answer: "Соотвествующий",
-        period: 1
-    },{
-        id: 10,
-        questions: "According to",
-        answer: "Согласно",
-        period: 1
-    },{
-        id: 11,
-        questions: "Arise",
-        answer: "Возникать",
-        period: 1
-    },{
-        id: 12,
-        questions: "Identity",
-        answer: "Идентичный",
-        period: 1
-    },{
-        id: 13,
-        questions: "Occur",
-        answer: "Происходить",
-        period: 1
-    },{
-        id: 14,
-        questions: "Fraction",
-        answer: "Дробь",
-        period: 1
-    },{
-        id: 15,
-        questions: "Theory",
-        answer: "Теория",
-        period: 1
-    }
+    },
 ]
 
 export const periodsEnglish = [{name: "Words", slug: 1}]
