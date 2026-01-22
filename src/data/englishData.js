cd60cff0dfaa64b8c55b2b4eb1d71041a500fd5b
Islam <ikabden04@gmail.com>
2026-01-22 17:12:31 +0500

    New 5 words are uploaded

--- src/data/englishData.js
+++ src/data/englishData.js
@@ -1,55 +1,80 @@
 export const questionsEnglish = [
-    {
-        id: 1,
-        questions: "Bulk up",
-        answer: "Качать мыщцы",
-        period: 1
-    },{
-        id: 2,
-        questions: "Praise",
-        answer: "Хвалить",
-        period: 1
-    },{
-        id: 3,
-        questions: "Aware",
-        answer: "Осведомленный",
-        period: 1
-    },{
-        id: 4,
-        questions: "Irresstible",
-        answer: "Неотрозимый",
-        period: 1
-    },{
-        id: 5,
-        questions: "Seashare",
-        answer: "Берег моря",
-        period: 1
-    },{
-        id: 5,
-        questions: "Adult",
-        answer: "Ересек",
-        period: 1
-    },{
-        id: 5,
-        questions: "Herring",
-        answer: "Майшабақ",
-        period: 1
-    },{
-        id: 5,
-        questions: "Gull",
-        answer: "Шағала",
-        period: 1
-    },{
-        id: 5,
-        questions: "Cardboard",
-        answer: "Картон",
-        period: 1
-    },{
-        id: 5,
-        questions: "Beak",
-        answer: "тұмсық",
-        period: 1
-    },
+  {
+    id: 1,
+    questions: 'Bulk up',
+    answer: 'Качать мыщцы',
+    period: 1
+  }, {
+    id: 2,
+    questions: 'Praise',
+    answer: 'Хвалить',
+    period: 1
+  }, {
+    id: 3,
+    questions: 'Aware',
+    answer: 'Осведомленный',
+    period: 1
+  }, {
+    id: 4,
+    questions: 'Irresstible',
+    answer: 'Неотрозимый',
+    period: 1
+  }, {
+    id: 5,
+    questions: 'Seashare',
+    answer: 'Берег моря',
+    period: 1
+  }, {
+    id: 6,
+    questions: 'Adult',
+    answer: 'Ересек',
+    period: 1
+  }, {
+    id: 7,
+    questions: 'Herring',
+    answer: 'Майшабақ',
+    period: 1
+  }, {
+    id: 8,
+    questions: 'Gull',
+    answer: 'Шағала',
+    period: 1
+  }, {
+    id: 9,
+    questions: 'Cardboard',
+    answer: 'Картон',
+    period: 1
+  }, {
+    id: 10,
+    questions: 'Beak',
+    answer: 'тұмсық',
+    period: 1
+  }, {
+    id: 11,
+    questions: 'discover',
+    answer: 'Табу',
+    period: 1
+  }, {
+    id: 12,
+    questions: 'Exaggerate',
+    answer: 'Асыра көрсету',
+    period: 1
+  }, {
+    id: 13,
+    questions: 'Abundant',
+    answer: 'Көп мөлшерде',
+    period: 1
+  }, {
+    id: 14,
+    questions: 'Crave',
+    answer: 'Қалау, тілеу',
+    period: 1
+  }, {
+    id: 15,
+    questions: 'Scarce',
+    answer: 'Сирек',
+    period: 1
+  }
 ]
 
-export const periodsEnglish = [{name: "Words", slug: 1}]
+export const periodsEnglish = [{ name: 'Words', slug: 1 }]
